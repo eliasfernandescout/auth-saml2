a93f2cc86792db77d338936e560435c55d69beae
eliasfernandescout <eliasfernandescout@gmail.com>
2024-10-03 11:27:29 -0300

    first commit

---
+++ src/main/java/javalab/auth_saml2/AuthSaml2Application.java
@@ -0,0 +1,13 @@
+package javalab.auth_saml2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSaml2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSaml2Application.class, args);
+	}
+
+}
---
+++ src/test/java/javalab/auth_saml2/AuthSaml2ApplicationTests.java
@@ -0,0 +1,13 @@
+package javalab.auth_saml2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSaml2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
